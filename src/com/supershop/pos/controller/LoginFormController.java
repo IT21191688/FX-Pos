7829ecd5974a3b2551225c921ab9c5c32b8dcd1e
IT21191688 <IT21191688@my.sliit.lk>
2023-07-20 17:12:27 +0530

    user register success

--- src/com/supershop/pos/controller/LoginFormController.java
+++ src/com/supershop/pos/controller/LoginFormController.java
@@ -1,4 +1,44 @@
 package com.supershop.pos.controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginFormController {
+
+    public AnchorPane context;
+    public JFXTextField txtEmail;
+    public JFXTextField txtPassword;
+    public JFXButton btnLogin;
+    public JFXButton btnSignUp;
+
+    public void btnLoginOnAction(ActionEvent event) {
+    }
+
+    public void btnSignUpOnAction(ActionEvent event) throws IOException {
+
+
+        setUi("SignupForm");
+
+
+    }
+
+    private void setUi(String url) throws IOException {
+
+        Stage stage=(Stage)context.getScene().getWindow();
+
+        stage.setScene(
+                new Scene(FXMLLoader.load(getClass().getResource("../view/"+url+".fxml")))
+        );
+
+        stage.centerOnScreen();
+
+    }
+
 }
--- src/com/supershop/pos/controller/SignupFormController.java
+++ src/com/supershop/pos/controller/SignupFormController.java
@@ -1,6 +1,74 @@
 package com.supershop.pos.controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class SignupFormController {
 
 
+    public AnchorPane context;
+    public JFXTextField txtEmail;
+    public JFXTextField txtPassword;
+
+    public void btnRegisterOnAction(ActionEvent event) {
+
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/shop_pos","root","1234");
+
+            String sql="INSERT INTO user VALUES (?,?)";
+
+
+            PreparedStatement preparedStatement=connection.prepareStatement((sql));
+            preparedStatement.setString(1,txtEmail.getText());
+            preparedStatement.setString(2,txtPassword.getText());
+
+
+            if(preparedStatement.executeUpdate()>0){
+
+                new Alert(Alert.AlertType.CONFIRMATION,"User Saved").show();
+
+            }else{
+                new Alert(Alert.AlertType.WARNING,"Try Again").show();
+            }
+
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR,e.getMessage()).show();
+        }
+
+
+    }
+
+    public void btnLoginPageOnAction(ActionEvent event) throws IOException {
+
+        setUi("LoginForm");
+
+    }
+
+
+
+    private void setUi(String url) throws IOException {
+
+        Stage stage=(Stage)context.getScene().getWindow();
+        stage.setScene(
+                new Scene(FXMLLoader.load(getClass().getResource("../view/"+url+".fxml")))
+        );
+        stage.centerOnScreen();
+
+    }
 }
